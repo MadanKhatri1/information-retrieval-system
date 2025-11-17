fb0ce3b22e351f40f11cb063f11c88d149ba169a
Madan Khatri <madankh00123@gmail.com>
2025-11-17 19:11:15 +0545

    add requirements file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI-IR-System",
+    version="0.0.0",
+    author="Madan Khatri",
+    author_email="madankh00123@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
